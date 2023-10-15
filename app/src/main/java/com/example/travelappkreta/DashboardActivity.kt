5ab1505614346ca35cb809f975251ed84cbac787
akhmadmaulanaakhsan <maulanaakhsan10@gmail.com>
2023-10-15 22:03:25 +0700

    four commit

---
+++ app/src/main/java/com/example/travelappkreta/DashboardActivity.kt
@@ -0,0 +1,64 @@
+package com.example.travelappkreta
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var btnTambahPesan: Button
+    private lateinit var date_Picker: CalendarView
+    private lateinit var textViewRencanaPerjalanan: TextView
+    private lateinit var lastTravelData: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+
+        // Inisialisasi komponen
+        btnTambahPesan = findViewById(R.id.btnTambahPesan)
+        date_Picker = findViewById(R.id.date_Picker)
+        textViewRencanaPerjalanan = findViewById(R.id.textViewRencanaPerjalanan)
+        lastTravelData = findViewById(R.id.lastTravelData)
+
+        // Set listener untuk tombol tambah pesan
+        btnTambahPesan.setOnClickListener {
+            val intent = Intent(this, InputRencanaPerjalananActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Set listener untuk CalendarView
+        date_Picker.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            // Di sini Anda dapat menangani perubahan tanggal yang dipilih oleh pengguna
+            // Misalnya, tampilkan data perjalanan terakhir berdasarkan tanggal yang dipilih
+            val selectedDate = "$dayOfMonth/${month + 1}/$year"
+            updateLastTravelData(selectedDate)
+        }
+
+        // Mendapatkan data dari Intent
+        val asal = intent.getStringExtra("ASAL")
+        val tujuan = intent.getStringExtra("TUJUAN")
+        val kelasKereta = intent.getStringExtra("KELAS_KERETA")
+        val selectedDate = intent.getStringExtra("SELECTED_DATE")
+        val totalHarga = intent.getIntExtra("TOTAL_HARGA", 0)
+
+        // Gunakan data sesuai kebutuhan Anda
+        // ...
+
+        // Contoh tampilan data pada TextView
+        val textViewData = findViewById<TextView>(R.id.textViewData)
+        textViewData.text = "Asal: $asal\nTujuan: $tujuan\nKelas Kereta: $kelasKereta\nTanggal: $selectedDate\nTotal Harga: $totalHarga"
+    }
+
+
+    }
+
+    private fun updateLastTravelData(selectedDate: String) {
+        // Di sini, Anda dapat mengambil dan menampilkan data perjalanan terakhir berdasarkan tanggal
+        // Contoh: lastTravelData.text = "Data terakhir: [Tanggal, Asal, Tujuan, Paket]"
+        // Gantilah dengan logika pengambilan data yang sesuai dengan proyek Anda.
+    }
+}
---
+++ app/src/main/java/com/example/travelappkreta/InputRencanaPerjalananActivity.kt
@@ -0,0 +1,164 @@
+package com.example.travelappkreta
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+data class PaketTambahan(val nama: String, val deskripsi: String)
+
+class InputRencanaPerjalananActivity : AppCompatActivity() {
+
+    private val stasiunList = listOf("Stasiun Jakarta", "Stasiun Bandung", "Stasiun Surabaya")
+    private val kelasKeretaList = listOf("Ekonomi", "Bisnis", "Eksekutif")
+    private val paketTambahanList = listOf(
+        PaketTambahan("Paket 1", "Deskripsi Paket 1"),
+        PaketTambahan("Paket 2", "Deskripsi Paket 2"),
+        PaketTambahan("Paket 3", "Deskripsi Paket 3"),
+        PaketTambahan("Paket 4", "Deskripsi Paket 4"),
+        PaketTambahan("Paket 5", "Deskripsi Paket 5"),
+        PaketTambahan("Paket 6", "Deskripsi Paket 6"),
+        PaketTambahan("Paket 7", "Deskripsi Paket 7")
+    )
+
+    private val hargaKelasKeretaList = listOf(100, 150, 200)
+    private val hargaPaketTambahanList = listOf(10, 15, 20, 25, 30, 35, 40)
+
+    private lateinit var spinnerAsal: Spinner
+    private lateinit var spinnerTujuan: Spinner
+    private lateinit var spinnerClassKereta: Spinner
+    private lateinit var horizontalScrollViewPaket: HorizontalScrollView
+    private lateinit var llToggleButtons: LinearLayout
+    private lateinit var textViewDetailPaket: TextView
+    private lateinit var textViewHarga: TextView
+    private lateinit var btnPesan: Button
+    private lateinit var datePicker: DatePicker
+
+    private var totalHarga: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinnerAsal = findViewById(R.id.spinnerAsal)
+        spinnerTujuan = findViewById(R.id.spinnerTujuan)
+        spinnerClassKereta = findViewById(R.id.spinnerClassKereta)
+        horizontalScrollViewPaket = findViewById(R.id.horizontalScrollViewPaket)
+        llToggleButtons = findViewById(R.id.llToggleButtons)
+        textViewDetailPaket = findViewById(R.id.textViewDetailPaket)
+        textViewHarga = findViewById(R.id.textViewHarga)
+        btnPesan = findViewById(R.id.btnPesan)
+        datePicker = findViewById(R.id.datePicker)
+
+        // Setup Adapter untuk Spinner Asal, Tujuan, dan Kelas Kereta
+        setupSpinner(spinnerAsal, stasiunList)
+        setupSpinner(spinnerTujuan, stasiunList)
+        setupSpinner(spinnerClassKereta, kelasKeretaList)
+
+        // Setup Toggle Buttons
+        setupToggleButtons(llToggleButtons, paketTambahanList)
+
+        // Setup Listener untuk perubahan pada Spinner dan Toggle Buttons
+        setupListeners()
+
+        // Setup Button Pesan
+        // Setup Button Pesan
+        btnPesan.setOnClickListener {
+            // Mendapatkan data yang ingin Anda kirim
+            val selectedAsal = spinnerAsal.selectedItem.toString()
+            val selectedTujuan = spinnerTujuan.selectedItem.toString()
+            val selectedClassKereta = spinnerClassKereta.selectedItem.toString()
+            val selectedDate = "${datePicker.dayOfMonth}/${datePicker.month + 1}/${datePicker.year}"
+
+            // Mengirim data ke DashboardActivity
+            val intent = Intent(this, DashboardActivity::class.java).apply {
+                putExtra("ASAL", selectedAsal)
+                putExtra("TUJUAN", selectedTujuan)
+                putExtra("KELAS_KERETA", selectedClassKereta)
+                putExtra("SELECTED_DATE", selectedDate)
+                putExtra("TOTAL_HARGA", totalHarga)
+            }
+
+            // Memulai aktivitas DashboardActivity dengan membawa data
+            startActivity(intent)
+        }
+    }
+
+    private fun setupSpinner(spinner: Spinner, data: List<String>) {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, data)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+    }
+
+    private fun setupToggleButtons(group: LinearLayout, data: List<PaketTambahan>) {
+        for (i in data.indices) {
+            val toggleButton = ToggleButton(this)
+            toggleButton.text = data[i].nama
+            toggleButton.layoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            group.addView(toggleButton)
+        }
+    }
+
+    private fun setupListeners() {
+        spinnerAsal.onItemSelectedListener = SpinnerItemSelectedListener()
+        spinnerTujuan.onItemSelectedListener = SpinnerItemSelectedListener()
+        spinnerClassKereta.onItemSelectedListener = SpinnerItemSelectedListener()
+
+        for (i in 0 until llToggleButtons.childCount) {
+            val toggleButton = llToggleButtons.getChildAt(i) as ToggleButton
+            toggleButton.setOnCheckedChangeListener { _, isChecked ->
+                updateTotalHarga()
+                updateDetailPaket()
+            }
+        }
+    }
+
+    private fun updateTotalHarga() {
+        totalHarga = 0
+
+        // Harga berdasarkan kelas kereta
+        totalHarga += hargaKelasKeretaList[spinnerClassKereta.selectedItemPosition]
+
+        // Harga berdasarkan paket tambahan yang dipilih
+        for (i in 0 until llToggleButtons.childCount) {
+            val toggleButton = llToggleButtons.getChildAt(i) as ToggleButton
+            if (toggleButton.isChecked) {
+                totalHarga += hargaPaketTambahanList[i]
+            }
+        }
+
+        // Tampilkan total harga
+        textViewHarga.text = "Harga: $totalHarga"
+    }
+
+    private fun updateDetailPaket() {
+        val selectedPakets = mutableListOf<String>()
+
+        // Tambahkan paket tambahan yang dipilih ke dalam list
+        for (i in 0 until llToggleButtons.childCount) {
+            val toggleButton = llToggleButtons.getChildAt(i) as ToggleButton
+            if (toggleButton.isChecked) {
+                selectedPakets.add(paketTambahanList[i].deskripsi)
+            }
+        }
+
+        // Tampilkan detail paket di textViewDetailPaket
+        textViewDetailPaket.text = "Detail Paket: ${selectedPakets.joinToString(", ")}"
+    }
+
+    inner class SpinnerItemSelectedListener : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+            updateTotalHarga()
+        }
+
+        override fun onNothingSelected(parent: AdapterView<*>?) {
+            // Do nothing
+        }
+    }
+
+
+}
